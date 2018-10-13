385adacc3a4933fb9887c8beb1d8623df1630fb4
allaoc <angusli98@gmail.com>
2018-10-13 18:24:26 -0400

    New signup module

---
+++ signup.py
@@ -0,0 +1,55 @@
+import sys
+import hashlib
+import os
+from guizero import App, Text, TextBox, PushButton, Window
+def adduser():
+	passhashed = hashlib.sha256(password.value.encode()).hexdigest()
+	if not doctors:
+		towrite = "%s\t%s\t%s" % (name.value,username.value,passhashed)
+	else:
+		towrite = "\n%s\t%s\t%s" % (name.value,username.value,passhashed)
+	with open(FILENAME, "a") as d:
+		d.write(towrite)
+def checkgood():
+	if username.value in usernames:
+		success.value = "This username is already taken. Please try another."
+	else:
+		success.value = "Registration successful!"
+		app.disable()
+		adduser()
+def allfilled():
+	if (name.value != "" and username.value != "" and password.value != "" and app.enabled):
+		confirm.enable()
+	else:
+		confirm.disable()
+FILENAME = "doctors.txt"
+doctors = []
+usernames = []
+passhashes = []
+try:
+	with open(FILENAME) as d:
+		for line in d:
+			fields = line.split("\t")
+			doctors.append(fields[0])
+			usernames.append(fields[1])
+			passhashes.append(fields[2].strip())
+except:
+	d = open(FILENAME,"w+")
+	d.close()
+app = App(title = "New user registration")
+intro = Text(app, text = "Welcome to Litlitlit!\n")
+prompt1 = Text(app, text = "Please enter your name (First Last).")
+name = TextBox(app, width = 20)
+prompt2 = Text(app, text = "Please enter your username.")
+username = TextBox(app, width = 20)
+prompt3 = Text(app, text = "Please enter your password.")
+password = TextBox(app, width = 20)
+password.tk.config(show="*")
+txt2 = Text(app, text = name.value)
+confirm = PushButton(app, text = "Confirm registration", command = checkgood)
+confirm.disable()
+success = Text(app, text = "")
+if app.enabled:
+	success.repeat(100, allfilled)
+app.display()
+
