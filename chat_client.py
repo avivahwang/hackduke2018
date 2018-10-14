9fa345deaeade82c2d9302ece2c3403457bf67bc
allaoc <angusli98@gmail.com>
2018-10-14 07:37:08 -0400

    Added GUI for chat_client

--- chat_client.py
+++ chat_client.py
@@ -7,6 +7,7 @@ Created on Oct 13, 2018
 import socket
 from threading import *
 import threading
+from guizero import App, Text, TextBox, PushButton
 
 HOST = '127.0.0.1'
 PORT = 65432
@@ -14,24 +15,52 @@ PORT = 65432
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 def execute_client():
-    print('Type "exit" at any time to leave this chat')
-
-    while True:
-        message = input("What is your message? ")
-        s.send(message.encode('ascii'))
-        if message == 'exit':
-            break
-        data = s.recv(4096)
-        if data.decode('ascii') == 'exit':
-            print('Doctor has ended the chat session')
-            break
-        print('Received message from doctor:', data.decode('ascii'))
-    s.close()
+	def push():
+		def checknew():
+			data = s.recv(4096)
+			history1.value = history2.value
+			history2.value = history3.value
+			history3.value = history4.value
+			history4.value = "Doctor: %s" % (data.decode('ascii'))
+			if data.decode('ascii') == 'exit':
+				prompt.text = "Doctor has ended the chat session"
+				app.disable()
+		s.send(message.value.encode('ascii'))
+		if message.value == 'exit':
+			app.destroy()
+		history1.value = history2.value
+		history2.value = history3.value
+		history3.value = history4.value
+		history4.value = "You: %s" % (message.value)
+		message.clear()
+		checknew()
+	#print('Type "exit" at any time to leave this chat')
+	app = App(title = "Chat")
+	toptext = Text(app, text = 'Type "exit" at any time to leave this chat\n')
+    #while True:
+	#message = input("What is your message? ")
+	history1 = Text(app, text = "")
+	history2 = Text(app, text = "")
+	history3 = Text(app, text = "")
+	history4 = Text(app, text = "")
+	prompt = Text(app, text = "What is your message?")
+	message = TextBox(app, width = 25)
+	goahead = PushButton(app, text = "Send", command = push)
+	app.display()
+	s.close()
+	#s.send(message.encode('ascii'))
+	#if message == 'exit':
+		#break
+	#data = s.recv(4096)
+	#if data.decode('ascii') == 'exit':
+	#	print('Doctor has ended the chat session')
+	#	break
+	#print('Received message from doctor:', data.decode('ascii'))
 
 def Main():
-    s.connect((HOST,PORT))
-    this_thread = Thread(target=execute_client)
-    this_thread.start()
+	s.connect((HOST,PORT))
+	this_thread = Thread(target=execute_client)
+	this_thread.start()
 
 if __name__ == '__main__':
-    Main()
+	Main()
