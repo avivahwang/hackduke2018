48d5cc83868a7664201835d2589ff0ee563a1f72
Ethan Ho <eh154@duke.edu>
2018-10-14 05:58:50 -0400

    Updated model to account for whether or not user is doctor or patient

--- models.py
+++ models.py
@@ -4,6 +4,8 @@ from pymodm import fields, MongoModel
 class User(MongoModel):
     # email = fields.EmailField()  # because primary_key is True,
     # we will need to query this field using the label _id
-    name = fields.CharField(primary_key=True)
+    username = fields.CharField(primary_key=True)
+    name = fields.CharField(blank=True)
+    doctor_status = fields.BooleanField()
     doctor = fields.CharField(blank=True)
     password = fields.CharField()
