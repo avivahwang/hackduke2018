616df977a9edc80dfdbf763e8d90d8fe86ec8354
avivahwang <avivahwang@yahoo.com>
2018-10-14 01:26:34 -0400

    Basic chat in console (run 'chat.py' then run 'chat_client.py')

---
+++ chat.py
@@ -0,0 +1,40 @@
+'''
+Created on Oct 13, 2018
+
+@author: avivahwang
+'''
+
+import socket
+from _thread import *
+import threading
+
+print_lock = threading.Lock()
+
+def new_thread(conn):
+    while True:
+        data = conn.recv(4096)
+        if not data:
+            print_lock.release()
+            break
+        print 'Received message from patient:', data.decode('ascii')
+        doc_message = raw_input("Doctor, what is your message? ")
+        conn.send(doc_message.encode('ascii'))
+    conn.close()
+
+def Main():
+    HOST = '127.0.0.1' #local host
+    PORT = 65432
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST,PORT))
+    #s.bind((socket.gethostname(), PORT))
+    s.listen(5)
+
+    while True:
+        conn, addr = s.accept()
+        print_lock.acquire()
+        #print('Connected by', addr)
+        start_new_thread(new_thread,(conn,))
+        
+if __name__ == '__main__':
+    Main()
---
+++ chat_client.py
@@ -0,0 +1,28 @@
+'''
+Created on Oct 13, 2018
+
+@author: avivahwang
+'''
+
+import socket
+
+def Main():
+    HOST = '127.0.0.1'
+    PORT = 65432
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST,PORT))
+    while True:
+        message = raw_input("What is your message? ")
+        s.send(message.encode('ascii'))
+        data = s.recv(4096)
+        print 'Received message from doctor:', data.decode('ascii')
+        ask = raw_input("Do you want to exit this chat? (y/n) ")
+        if ask == 'y':
+            break
+        else:
+            continue
+    s.close()
+    
+if __name__ == '__main__':
+    Main()
