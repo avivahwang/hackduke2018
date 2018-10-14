63afae4053237a1fff9f7107bc2566a06fa90bb7
Ethan Ho <eh154@duke.edu>
2018-10-14 06:00:09 -0400

    Added methods to save user info

---
+++ user_functions.py
@@ -0,0 +1,21 @@
+from signupPatient import *
+from pymodm import connect
+import models
+import datetime
+
+connect("mongodb://vcm-6782.vm.duke.edu:27017/test")
+
+def create_patient(username, doctor, password):
+    u = models.User(username, [], False, doctor, password)
+    u.save()
+    print('Patient created!')
+
+
+def create_doctor(username, name, password):
+    u = models.User(username, name, True, '', password)
+    u.save()
+    print('Doctor created!')
+
+
+def record_time():
+    pass
