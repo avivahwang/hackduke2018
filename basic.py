524c47f6650557c44d1860ea0a98a627a4028f08
Ethan Ho <eh154@duke.edu>
2018-10-14 06:49:51 -0400

    Separated creating users into creating patients and doctors

--- basic.py
+++ basic.py
@@ -1,13 +1,35 @@
-from main import *
+import datetime
+import models
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
+from main import *
+
 app = Flask(__name__)
+CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.route("/api/user_base", methods=["POST"])
-def user_data():
-    r = request.get_json()
-    name = r["name"]
-    doctor = r["doctor"]
-    password = r["password"]
-    user =  create_user(name, doctor, password)
-    return "User data receieved"
+@app.route("/api/patient_base", methods=["POST"])
+    def patient_data():
+        try:
+            r = request.get_json()
+            username = r["username"]
+            doctor = r["doctor"]
+            password = r["password"]
+            patient = create_patient(username, doctor, password)
+            return "Patient data added."
+        except:
+            return "Not a valid input!"
+
+
+@app.route("/api/doctor_base", methods=["POST"])
+    def doctor_data():
+        try:
+            r = request.get_json()
+            username = r["username"]
+            name = r["name"]
+            password = r["password"]
+            doctor = create_doctor(username, name, password)
+            return "Doctor data added."
+        except:
+            return "Not a valid input!"
